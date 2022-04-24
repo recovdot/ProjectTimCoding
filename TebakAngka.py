f9c1405b60b2fb987aaa903b1d8c665ea697aed7
Tomoe Isozaki <recovdot@gmail.com>
2022-04-24 12:36:07 +0800

    Up to repo

---
+++ TebakAngka.py
@@ -0,0 +1,18 @@
+import random as rand
+
+acak = rand.randint(0, 10)
+
+if (acak >= 5):
+    print("Clue nih: Angka lebih dari 5")
+elif (acak <= 5):
+    print("Clue nih: Angka kurang dari 5")
+
+# Aktifkan kode dibawah ini untuk debugging Angka
+# print(f"Angka Debug: {acak}\n")
+
+inputAngka = input("Hayooo tebak Angkanya: ")
+
+if(inputAngka == f"{acak}"):
+    print("KAMU BENAR, Selamat ya udah ga Jomblo lagi :P")
+else:
+    print("Yahaha Salah, kasian Jomblo :P")
